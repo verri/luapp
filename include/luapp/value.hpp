af48a67ab7249f97adf7b4ced471590421a8b4ca
Filipe Verri <filipeverri@gmail.com>
2020-04-05 12:15:33 -0300

    Added type.hpp.

---
+++ include/luapp/value.hpp
@@ -0,0 +1,19 @@
+#ifndef LUAPP_VALUE_HPP_INCLUDED
+#define LUAPP_VALUE_HPP_INCLUDED
+
+extern "C"
+{
+#include <lua.h>
+}
+
+namespace lua
+{
+
+class value
+{
+
+};
+
+} // namespace lua
+
+#endif // LUAPP_VALUE_HPP_INCLUDED
